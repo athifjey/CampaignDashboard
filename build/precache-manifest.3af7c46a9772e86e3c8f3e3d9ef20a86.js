b571d4ab3170bd64ba703f1b883c5644b83c9003
mohammed.athif <mohammed.athif@tagitmobile.com>
2019-10-21 23:49:34 +0530

    Production build added

---
+++ build/precache-manifest.3af7c46a9772e86e3c8f3e3d9ef20a86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec7dfa4028be7a7e8de6f279674a50d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a977911236666220547b",
+    "url": "/static/css/main.2e4da559.chunk.css"
+  },
+  {
+    "revision": "bed6198bbb696bd4f2f5",
+    "url": "/static/js/2.8ba895fb.chunk.js"
+  },
+  {
+    "revision": "a977911236666220547b",
+    "url": "/static/js/main.24ef50ab.chunk.js"
+  },
+  {
+    "revision": "27dc5953f34bf9b279a9",
+    "url": "/static/js/runtime-main.65ce77d4.js"
+  }
+]);
